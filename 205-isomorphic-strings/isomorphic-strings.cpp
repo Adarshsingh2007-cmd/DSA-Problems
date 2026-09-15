a074711cd7c223e5ec7a22a283686b8c36fd9a27
Adarshsingh2007-cmd <adarshsingh10117@gmail.com>
2026-09-15 20:47:19 +0530

    Time: 3 ms (39.38%) | Memory: 9.4 MB (57.76%) - LeetSync

---
+++ 205-isomorphic-strings/isomorphic-strings.cpp
@@ -0,0 +1,30 @@
+class Solution {
+public:
+    bool isIsomorphic(string s, string t) {
+        int m = s.size();
+        int n = t.size();
+        if(m!=n)
+        {
+            return false;
+        }
+        unordered_map<char,char>mp1;
+        unordered_map<char,char>mp2;
+        for(int i=0;i<m;i++)
+        {if(mp1.find(s[i])==mp1.end())
+        {
+         mp1[s[i]] = t[i];
+        }else if(mp1[s[i]]!=t[i])
+        {
+            return false;
+        }  
+        if(mp2.find(t[i])==mp2.end())
+        {
+          mp2[t[i]] = s[i]; 
+        }else if(mp2[t[i]]!=s[i])
+        {
+            return false;
+        }   
+
+        }return true;
+    }
+};
